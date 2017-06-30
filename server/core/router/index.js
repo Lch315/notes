469da731a2487813a026ae13d50fe034ae28ebf5
刘春晖 <liuchunhui@zhongan.com>
2017-06-30 21:22:15 +0800

    router middleware

--- server/core/router/index.js
+++ server/core/router/index.js
@@ -6,12 +6,19 @@
 
 import Router from './Router'
 import match from './match'
+import applyMiddleware from './applyMiddleware'
 
 const routerRegister = register => {
   return (ctx, next) => {
-    console.log('ctx:', ctx)
-    console.log('match:', match(ctx, Router.regActuator(register)))
-    ctx.body = { status: 200, data: { accountId: 123456 } }
+    let route = match(ctx, Router.regActuator(register))
+
+    if (route) {
+      let { controller, middlewares, params } = route
+
+      applyMiddleware([ ...middlewares, controller ])(ctx, params)
+    }
+
+    //ctx.body = { status: 200, data: { accountId: 123456 } }
 
     next()
   }
